92929697dac19eaa5e81807a67cb17f09f4217fa
StvAlxnder <smamanie@unjbg.edu.pe>
2025-05-30 17:58:19 -0500

    Actualiza ejercicio3: implementación de función esPositivo y función serieFibonacci.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 using namespace std;
+int esPositivo(int val);
+int serieFibonacci(int val);
+
 int main(){
-    int nTerminos, suma, a, b, c;
+    int nTerminos, suma;
     cout<<"Ingrese el numero de terminos de la serie de Fibonacci que desea generar: "; cin>>nTerminos;
-    a = 0;
-    b = 1;
-    suma = 0;
-    if(nTerminos<=0){
-        cout<<"Error: Ingrese un número positivo.";
+    if(esPositivo(nTerminos)==0)//llamada a la función positivo
         return 1;
+    suma = serieFibonacci(nTerminos);//llamada a la función serieFibonacci
+    cout<<"\nLa suma de los terminos es: "<<suma;
+    return 0;
+}
+
+//definición de la función esPositivo
+int esPositivo(int val){
+    if(val>0)
+        return 1;
+    else{
+        cout<<"Error: el numero ingresado no es positivo.\n";
+        return 0;
     }
-    for(int i=1; i<=nTerminos; i++){
+}
+
+//definición de la función serieFibonacci
+int serieFibonacci(int val){
+    int c;          //
+    int a = 0;      // variables
+    int b = 1;      // locales
+    int suma = 0;   //
+    cout<<"Serie de Fibonacci: ";
+    for(int i = 1; i<=val; i++){
         cout<<a<<" ";
         suma = suma + a;
         c = a + b;
         a = b;
         b = c;
     }
-    cout<<"\nLa suma de los terminos es: "<<suma;
-    return 0;
+    return suma;
 }
