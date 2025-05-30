7ac9f117010bc878e71b1485f787d8d62cf4ee3f
StvAlxnder <smamanie@unjbg.edu.pe>
2025-05-30 17:29:15 -0500

    Actualiza ejercicio3: agregar condición para que el número de términos sea positivo.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 int main(){
     int nTerminos, suma, a, b, c;
-    cout<<"Ingrese el numero de terminos: "; cin>>nTerminos;
+    cout<<"Ingrese el numero de terminos de la serie de Fibonacci que desea generar: "; cin>>nTerminos;
     a = 0;
     b = 1;
     suma = 0;
+    if(nTerminos<=0){
+        cout<<"Error: Ingrese un número positivo.";
+        return 1;
+    }
     for(int i=1; i<=nTerminos; i++){
         cout<<a<<" ";
         suma = suma + a;
