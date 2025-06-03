8f8658d33065bab19d60af6d9512b8b4653d5f10
StvAlxnder <smamanie@unjbg.edu.pe>
2025-06-02 20:04:33 -0500

    Actualiza ejercicio6: agrega función cantidadDivisores.

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
+int cantidadDivisores(int val);
 
 int main(){
-    int num, divisores;
+    int num;
     cout<<"Detector de numeros primos"<<endl;
     cout<<"Ingrese un numero: "; cin>>num;
-    divisores = 0;
-    for(int i = 1; i <= num; i++){
-        if(num % i == 0)
-            divisores++;
-    }
-    if(divisores != 2)
+    if(cantidadDivisores(num) != 2)//llamada a la función cantidadDivisores
         cout<<"El numero "<<num<<" NO es primo.";
     else
         cout<<"El numero "<<num<<" SI es primo.";
     return 0;
+}
+
+//definición de la función cantidadDivisores
+int cantidadDivisores(int val){
+    int divisores = 0;
+    for(int i = 1; i <= val; i++){
+        if(val % i == 0)
+            divisores++;
+    }
+    return divisores;
 }
