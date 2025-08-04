9ee707f01c39b304f9034894cfd69b1ee1876212
StvAlxnder <smamanie@unjbg.edu.pe>
2025-08-03 19:16:02 -0500

    Agrega ejercicio9.cpp: Calcula una suma especifica

---
+++ ejercicio9.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int factorial(int);
+void suma(float);
+
+int main(){
+    int n;
+    cout << "Ingrese el valor de n: ";
+    cin >> n;
+    suma (n);
+    return 0;
+}
+
+int factorial (int a){
+    int f;
+    f = 1;
+    for (int i=2; i<=a; i++){
+        f = f*i;
+    }
+    return f;
+}
+
+void suma (float b){
+    float S;
+    S = 0;
+    for (int i=1; i<=b; i++){
+        S = S + ((factorial(i)) / (2*i));
+    }
+    cout << "La suma es " << S << "." << endl;
+}
